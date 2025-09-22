b25d9880f6c2750e86ec7246e34b81dce37873fd
Gasquel <nthngadea@gmail.com>
2025-09-21 21:22:53 -0400

    fix: eliminar código innecesario en los componentes de la aplicación

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,34 @@
+"use client";
+import { useRouter } from "next/navigation";
 import { ModeToggle } from "./toggle-mode";
+import { Button } from "./ui/button";
 
 export function Header() {
+  const router = useRouter();
   return (
     <div className="fixed top-4 right-4 z-50">
-      <ModeToggle />
+      <nav
+        role="navigation"
+        aria-label="Primary Actions"
+        className="flex items-center gap-3"
+      >
+        <Button
+          variant="link"
+          className="px-3 py-1"
+          onClick={() => router.push("/signin")}
+        >
+          Sign In
+        </Button>
+
+        <Button
+          variant="link"
+          className="px-3 py-1"
+          onClick={() => router.push("/signup")}
+        >
+          Sign Up
+        </Button>
+        <ModeToggle />
+      </nav>
     </div>
   );
 }
