0b8058e3755d38cebe39bde5f8428ddf1658043e
Gasquel <nthngadea@gmail.com>
2025-09-30 11:09:53 -0400

    fix: eliminar temporalmente el padding inferior para evitar barras de desplazamiento

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -4,7 +4,9 @@ import { Header } from "./header";
 
 export function Main({ children }: { children: ReactNode }) {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    // ! temporarily remove bottom padding to avoid scrollbars
+    // <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 ">
       <Header />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {children}
